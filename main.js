75b2536cbf9bf4218db7de28a01dcf053fd49762
Harley-gv <hegv1995@gmail.com>
2022-04-24 20:12:54 -0500

    avaces ecommerce sumando productos en el carrito

--- main.js
+++ main.js
@@ -16,25 +16,58 @@ closeCart.onclick = () => {
 
 //
 
-if (document.readyState == 'loading'){
+if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready);
-}else{
+} else {
     ready();
-} 
-
-// creando funciones
-function ready(){
-  //eliminar items del carrito
-  var removeCartButtons = document.getElementsByClassName('cart-remove');
-  console.log(removeCartButtons)
-  for (var i = 0; i < removeCartButtons.length; i++){
-       var buttton = removeCartButtons[i];
-       buttton.addEventListener('click',removeCartItem)
-  }
 }
 
-//
+// creando funciones
+function ready() {
+
+    var removeCartButtons = document.getElementsByClassName('cart-remove');
+    console.log(removeCartButtons)
+    for (var i = 0; i < removeCartButtons.length; i++) {
+        var button = removeCartButtons[i];
+        button.addEventListener('click', removeCartItem)
+    }
+    //Quantity changes
+    var quantityInputs = document.getElementsByClassName('cart-quantity')
+    for (var i = 0; i < quantityInputs.length; i++){
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+
+    }
+}
+
+// eliminar items del carrito
 function removeCartItem(e) {
-    var butttonCliked =  e.target;
-    butttonCliked.parentElement.remove()
+    var buttonClicked = e.target;
+    buttonClicked.parentElement.remove();
+    updatetotal();
+}
+//Quantity changes
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updatetotal()
+}
+
+//update total
+function updatetotal() {
+    var cartContent = document.getElementsByClassName('cart-content')[0];
+    var cartBoxes = cartContent.getElementsByClassName('cart-card');
+    var total = 0;
+    for (var i = 0; i < cartBoxes.length; i++) {
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+        var price  = parseFloat(priceElement.innerText.replace("$", ""))
+        var quantity = quantityElement.value;
+        total = total + (price * quantity);
+
+        document.getElementsByClassName('total-price')[0].innerText = "$" + total;
+    }
 }
